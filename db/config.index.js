54e31b884db95de2e4c364d43a9c4b07b6e5412b
sco7 <sco77.townson@gmail.com>
2018-07-24 13:07:47 +0100

    db config update

--- db/config.index.js
+++ db/config.index.js
@@ -1,5 +1,11 @@
-const pgp = require('pg-promise')({promiseLib: Promise});
-const config = require(`../config/config.${process.env.NODE_ENV}.js`);
-console.log(config);
+// const pgp = require('pg-promise')({promiseLib: Promise});
+// const config = require(`../config/config.${process.env.NODE_ENV}.js`);
+// console.log(config);
 
-module.exports = pgp(config.db);
+// module.exports = pgp(config.db);
+
+
+const pgp = require('pg-promise')({promiseLib: Promise});
+const config = process.env.DATABASE_URL ? process.env.DATABASE_URL : require(`./config/${process.env.NODE_ENV}.js`);
+
+module.exports = pgp(config);
