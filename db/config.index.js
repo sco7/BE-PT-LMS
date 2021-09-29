c8aff6ed5c756fba3952bc1b44752e6fe0d7412e
sco7 <34144749+sco7@users.noreply.github.com>
2021-09-29 18:13:09 +0100

    Update config.index.js
    Changed for deployment to Heroku

--- db/config.index.js
+++ db/config.index.js
@@ -1,15 +1,15 @@
-//required for running locally
+// required for running locally
+//const pgp = require('pg-promise')({promiseLib: Promise});
+//const config = require(`../config/config.${process.env.NODE_ENV}.js`);
+//console.log(config);
+
+//module.exports = pgp(config.db);
+
+
+
+
+// required for Heroku deployment
 const pgp = require('pg-promise')({promiseLib: Promise});
-const config = require(`../config/config.${process.env.NODE_ENV}.js`);
-console.log(config);
+const config = process.env.DATABASE_URL ? process.env.DATABASE_URL : require(`./config/${process.env.NODE_ENV}.js`);
 
-module.exports = pgp(config.db);
-
-
-
-
-//required for Heroku deployment
-// const pgp = require('pg-promise')({promiseLib: Promise});
-// const config = process.env.DATABASE_URL ? process.env.DATABASE_URL : require(`./config/${process.env.NODE_ENV}.js`);
-
-// module.exports = pgp(config);
+module.exports = pgp(config);
